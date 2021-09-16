e35118ba655739ab54316498c70379e1936c0901
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-16 09:20:20 +0200

    formatiranje

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants";
 import { selectToken } from "../../store/login/selectors";
-// import "./style.scss";
 
 const Header = () => {
   const token = useSelector(selectToken());
