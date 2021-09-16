5e5188a2ca53411668e2b9a9379718464e3cd5ae
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-16 09:17:41 +0200

    dodati stilovi za listanje filmova

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../constants";
 import { selectToken } from "../../store/login/selectors";
+// import "./style.scss";
 
 const Header = () => {
   const token = useSelector(selectToken());
--- src/components/Movies/MovieItem.js
+++ src/components/Movies/MovieItem.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
-
+import "./style.scss";
 const MovieItem = ({ movie }) => {
   return (
-    <li>
-      <div>{movie.description}</div>
-      <div>
-        <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+    <div>
+      <div className="movie_card" id="bright">
+        <div className="info_section">
+          <div className="movie_header">
+            <img className="locandina" src={movie.cover_image} alt="img" />
+            <h1>{movie.title}</h1>
+          </div>
+          <div className="movie_desc">
+            <p className="text">{movie.description}</p>
+          </div>
+          <div className="movie_social"></div>
+        </div>
+        <div className="blur_back bright_back"></div>
       </div>
-      <img src={movie.cover_image} alt="img" height="200px" width="200px"></img>
-    </li>
+    </div>
   );
 };
 
